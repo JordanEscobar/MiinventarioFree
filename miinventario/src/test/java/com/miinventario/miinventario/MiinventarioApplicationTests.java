7cd535920975eb57a5e60f18e477125110978f4b
jordanEscobar <jordanescosoto@gmail.com>
2025-08-23 14:31:28 -0400

    Creación proyecto back con Spring

---
+++ miinventario/src/test/java/com/miinventario/miinventario/MiinventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miinventario.miinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiinventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
